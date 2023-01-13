62b49d00bb7562e720967024083a2e0fb3742e61
Selectys <selectys.x@gmail.com>
2023-01-14 01:00:23 +0300

    DONE!

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,26 +1,24 @@
-import './index.scss'
-import './style.scss'
+import '/index.scss'
 
 document.querySelector('#app').innerHTML = `
-  <div>
     <header class="main-header">
-        <div class="layers">
+          <div class="layers">
             <div class="layer__header">
-                <div class="layers__caption">Welcome to Parallax</div>
-                <div class="layers__title">Fairy Forest</div>            
-            </div>  
+              <div class="layers__caption">Welcome to Parallax</div>
+              <div class="layers__title">Fairy Forest</div>
+            </div>
             <div class="layer layers__base"  style="background-image: url(/img/layer-base.png);"></div>
             <div class="layer layers__middle" style="background-image: url(/img/layer-middle.png);"></div>
-            <div class="layer layers__front" style="background-image: url(/img/layer-front.png);"></div>  
-        </div>
-    </header>
-    
-    <article class="main-article" style="background-image: url(/img/dungeon.jpg)">
-        <div class="main-article__content">
+            <div class="layer layers__front" style="background-image: url(/img/layer-front.png);"></div>
+          </div>
+        </header>
+
+        <article class="main-article" style="background-image: url(/img/dungeon.jpg)">
+          <div class="main-article__content">
             <h2 class="main-article__header">To be continued</h2>
             <p class="main-article__paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis dolor earum id incidunt magnam mollitia nihil, officiis velit? Cumque delectus, dolore esse fugit hic in iure laudantium mollitia natus odio veniam, vero. Lorem ipsum dolor sit amet, consectetur adipisicing elit... </p>
-        </div>
-        <div class="copy">© Parallax</div>
-    </article>
-  </div>
+          </div>
+          <div class="copy">© Parallax</div>
+        </article>
+    </div>
 `
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,5 +1,10 @@
-window.addEventListener("scroll", e=>{
-    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
+window.addEventListener('scroll', e => {
+    document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`)
 })
 
-console.log();
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.app',
+})
+
